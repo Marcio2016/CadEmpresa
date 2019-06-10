7e6a284e48a8375099154b5a4285bb7816c05b89
Marcio Tadeu <marcio.tadeu2@gmail.com>
2019-06-10 09:51:19 -0300

    Implementação das classes modelo, service, repository e util. Criação dos xmls de configuração do weld, persistência e messages.

--- src/main/java/br/com/mt/cadempresa/repository/EmpresaRepository.java
+++ src/main/java/br/com/mt/cadempresa/repository/EmpresaRepository.java
@@ -1,5 +1,35 @@
 package br.com.mt.cadempresa.repository;
 
-public class EmpresaRepository {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import br.com.mt.cadempresa.model.Empresa;
+
+public class EmpresaRepository implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityManager manager;
+
+	public Empresa findId(Long id) {
+		return manager.find(Empresa.class, id);
+	}
+
+	public List<Empresa> findAll() {
+		return manager.createQuery("from Empresa", Empresa.class).getResultList();
+	}
+
+	public Empresa save(Empresa empresa) {
+		return manager.merge(empresa);
+	}
+
+	public void delete(Empresa empresa) {
+		empresa = findId(empresa.getId());
+		manager.remove(empresa);
+	}
 
 }
--- src/main/java/br/com/mt/cadempresa/service/CadastroEmpresaService.java
+++ src/main/java/br/com/mt/cadempresa/service/CadastroEmpresaService.java
@@ -1,5 +1,27 @@
 package br.com.mt.cadempresa.service;
 
-public class CadastroEmpresaService {
+import java.io.Serializable;
 
+import javax.inject.Inject;
+
+import br.com.mt.cadempresa.model.Empresa;
+import br.com.mt.cadempresa.repository.EmpresaRepository;
+import br.com.mt.cadempresa.util.Transacional;
+
+public class CadastroEmpresaService implements Serializable {
+
+private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private EmpresaRepository empresasRepository;
+	
+	@Transacional
+	public void salvar(Empresa empresa) {
+		empresasRepository.save(empresa);
+	}
+	
+	@Transacional
+	public void excluir(Empresa empresa) {
+		empresasRepository.delete(empresa);
+	}
 }
