7fa4a1cda5436aba6b09a347a67c664bfbf4a4b8
Marcio Tadeu <marcio.tadeu2@gmail.com>
2019-06-15 20:36:14 -0300

    Implementação do modal para persistência de uma nova empresa, criação do template com principal.

--- src/main/java/br/com/mt/cadempresa/controller/EmpresaBean.java
+++ src/main/java/br/com/mt/cadempresa/controller/EmpresaBean.java
@@ -8,26 +8,63 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.mt.cadempresa.model.Empresa;
+import br.com.mt.cadempresa.model.TipoEmpresa;
 import br.com.mt.cadempresa.repository.EmpresaRepository;
+import br.com.mt.cadempresa.service.CadastroEmpresaService;
+import br.com.mt.cadempresa.util.FacesMessages;
 
 @Named
 @ViewScoped
 public class EmpresaBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
+	@SuppressWarnings("cdi-ambiguous-dependency")
 	@Inject
 	private EmpresaRepository empresaRepository;
 	
-	private List<Empresa> empresas;
+	@SuppressWarnings("cdi-ambiguous-dependency")
+	@Inject
+	private CadastroEmpresaService empresaService;
 	
-	public void findAllEmpresas(){
+	@SuppressWarnings("cdi-ambiguous-dependency")
+	@Inject
+	private FacesMessages messages;
+
+	private List<Empresa> empresas;
+
+	private Empresa empresaEdicao = new Empresa();
+
+
+	public void preparaNovoCadastro() {
+		empresaEdicao = new Empresa();
+	}
+	
+	public void salvar() {
+		empresaService.salvar(empresaEdicao);
+		findAllEmpresas();
+		messages.info("Empresa cadastrada");
+		
+	}
+	
+	public void findAllEmpresas() {
 		empresas = empresaRepository.findAll();
 	}
 
 	public List<Empresa> getEmpresas() {
 		return empresas;
-	}	
-	
+	}
+
+	public TipoEmpresa[] getTiposEmpresas() {
+		return TipoEmpresa.values();
+	}
+
+	public Empresa getEmpresaEdicao() {
+		return empresaEdicao;
+	}
+
+	public void setEmpresaEdicao(Empresa empresaEdicao) {
+		this.empresaEdicao = empresaEdicao;
+	}
 
 }
